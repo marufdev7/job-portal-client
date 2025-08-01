537ff945a6c962073c44a4006512d88c7ad77392
mrfhmd7 <mrf.hmd7@gmail.com>
2025-08-01 22:02:32 +0600

    job card add

--- src/pages/Home/Category/CategoryCard.jsx
+++ src/pages/Home/Category/CategoryCard.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const CategoryCard = ({ categoryItem }) => {
     const { category, jobs_available, img } = categoryItem;
     return (
-        <div class="bg-slate-100 hover:bg-slate-50 rounded-xl shadow-md hover:shadow-lg p-3 mt-4 flex items-center gap-2">
+        <div className="bg-slate-100 hover:bg-slate-50 rounded-xl shadow-md hover:shadow-lg p-3 mt-4 flex items-center gap-2">
             <img className='w-12 h-12' src={img} alt="" />
             <div>
-                <h3 class="font-semibold text hover:text-blue-500">{category}</h3>
-                <p class="text-sm text-gray-500 mt-1">{jobs_available} Jobs Available</p>
+                <h3 className="font-semibold text hover:text-blue-500">{category}</h3>
+                <p className="text-sm text-gray-500 mt-1">{jobs_available} Jobs Available</p>
 
             </div>
         </div>
--- src/pages/Home/HotJobs.jsx
+++ src/pages/Home/HotJobs.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const HotJobs = () => {
+
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/jobs')
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    }, [])
     return (
         <div>
-            
+            <h1>Hot jobs of the day</h1>
+            <h4>Search and connect with the right candidates faster.</h4>
+            <div>
+                {
+                    
+                }
+            </div>
         </div>
     );
 };
