f138756fe2e0a86b60326cc12317caf4bd9bef6e
mrfhmd7 <mrf.hmd7@gmail.com>
2025-08-01 21:53:36 +0600

    hot jobs add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Category from './Category/Category';
+import HotJobs from './HotJobs';
 
 const Home = () => {
     return (
         <div>
             <Banner />
-            <Category/>
+            <Category />
+            <HotJobs/>
         </div>
     );
 };
