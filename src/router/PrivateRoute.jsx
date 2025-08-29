317633804cf01795f12ef73904947a2e89ed9168
mrfhmd7 <mrf.hmd7@gmail.com>
2025-08-29 20:04:45 +0600

    create utils skeleton

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,5 +1,6 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
+import Skeleton from '../utils/Skeleton';
 
 
 
@@ -9,12 +10,7 @@ const PrivateRoute = ({ children }) => {
 
     if (loading) {
         return <>
-            <div className="flex w-52 flex-col gap-4">
-                <div className="skeleton h-32 w-full"></div>
-                <div className="skeleton h-4 w-28"></div>
-                <div className="skeleton h-4 w-full"></div>
-                <div className="skeleton h-4 w-full"></div>
-            </div>
+            <Skeleton/>
         </>
     }
 
