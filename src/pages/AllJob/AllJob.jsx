32d7aa78ca79f804aca1e110f472bbe000891558
mrfhmd7 <mrf.hmd7@gmail.com>
2025-08-29 20:21:42 +0600

    show jobs and use loading

--- src/pages/AllJob/AllJob.jsx
+++ src/pages/AllJob/AllJob.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import useJobs from '../../hooks/useJobs';
+import Skeleton from '../../utils/Skeleton';
+import HotJobsCard from '../Home/HotJobs/HotJobsCard';
 
 const AllJob = () => {
+    const { jobs, loading } = useJobs();
+    if (loading) {
+        return <Skeleton />
+    }
     return (
         <div>
             <h1 className='text-center text-4xl font-bold text-gray-700'>All Jobs</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
+                {
+                    jobs.map(job => <HotJobsCard
+                        key={job._id}
+                        job={job}
+                    />
+                    )
+                }
+            </div>
         </div>
     );
 };
