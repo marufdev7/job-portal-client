47c4faa752b6a33c3a6bbbf5646f5598e87104f2
mrfhmd7 <mrf.hmd7@gmail.com>
2025-08-29 21:13:52 +0600

    send sort value in custom hook and backend through api

--- src/pages/AllJob/AllJob.jsx
+++ src/pages/AllJob/AllJob.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useJobs from '../../hooks/useJobs';
 import Skeleton from '../../utils/Skeleton';
 import HotJobsCard from '../Home/HotJobs/HotJobsCard';
 
 const AllJob = () => {
-    const { jobs, loading } = useJobs();
+    const [sort, setSort] = useState(false);
+    const { jobs, loading } = useJobs(sort);
+    // console.log(sort);
     if (loading) {
         return <Skeleton />
     }
     return (
-        <div>
-            <h1 className='text-center text-4xl font-bold text-gray-700'>All Jobs</h1>
+        <div className='bg-slate-200'>
+            <h1 className='text-center text-4xl font-bold text-gray-700 py-4'>All Jobs</h1>
+            <div className='w-11/12 mx-auto py-5 p-3 flex items-center'>
+                <button
+                    onClick={() => setSort(!sort)}
+                    className={`btn btn-neutral ${sort && "btn-success"}`}
+                >
+                    {sort ? "Sorted by Salary" : " Sort by Salary"}
+                </button>
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
                 {
                     jobs.map(job => <HotJobsCard
